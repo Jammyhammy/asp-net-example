91982647260bbc9bf3bf1b490047bfd18c3c36e2
Jammyhammy <guvadilk@gmail.com>
2016-03-06 18:46:26 -0600

    DbContext, custom attributes, reports.

---
+++ Web-Five/WebFive/WebFive/Models/BaseReportModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebFive.CustomAttributes;
+using WebFive.Models.Reports;
+
+namespace WebFive.Models
+{
+    public abstract class BaseReportModel
+    {
+        /// <summary>
+        /// Gets and sets date radnge of report
+        /// </summary>
+        [DateRangeValidator]
+        public DateRangeModel DateRange { get; set; }
+    }
+}
